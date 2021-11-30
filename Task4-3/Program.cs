1e19d0e15571ddd599f9608594501b29c4de5209
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2021-11-30 19:47:47 +0300

    Добавьте файлы проекта.

---
+++ Task4-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_3
+{
+    class Program
+    {
+        // Найти количество квадратов размещенных на прямоугольнике
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите положительные числа A, B, C");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int counterA = 0;
+            int counterB = 0;            
+            while (a>=c)
+            {
+                a -= c;
+                counterA++;
+            }
+            while (b >= c)
+            {
+                b -= c;
+                counterB += counterA;
+            }            
+            Console.WriteLine(counterB);
+            Console.ReadKey();
+        }
+    }
+}
